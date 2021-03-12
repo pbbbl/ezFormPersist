caccc5492d38abd7526746f7b8214fce1e65f3ad
Tyler Grow <tylergrow@tylergrow.lan>
2021-03-11 22:45:16 -0700

    first commit

---
+++ src/formSaver.js
@@ -0,0 +1,54 @@
+import Cookies from 'js-cookie';
+import cleanId from './helpers/cleanId';
+import fsValidate from './helpers/fsValidate';
+export default () => {
+    runFormSaver();
+    function runFormSaver () {
+        var fieldEls = document.querySelectorAll( '[data-save-field]' );
+        fieldEls.forEach( function ( field, index ) {
+            var name = 'field_' + index;
+            var tag = field.tagName.toLowerCase();
+            if ( field && field.name ) {
+                name = field.name;
+            }
+            var pageId = cleanId();
+            var pageName = pageId + '__' + name;
+            var siteName = 'user__' + name;
+            var pageCookie = fsValidate( Cookies.get( pageName ) );
+            var siteCookie = fsValidate( Cookies.get( siteName ) );
+            var saved;
+            if ( pageCookie.valid ) {
+                saved = pageCookie;
+            }
+            else if ( siteCookie.valid ) {
+                saved = siteCookie;
+            }
+            if ( saved ) {
+                var savedValue = '';
+                if ( saved.value != null && saved.value != undefined ) {
+                    if ( saved.value == 'false' ) {
+                        savedValue = false;
+                    } else {
+                        savedValue = saved.value;
+                    }
+                    field.value = savedValue;
+                }
+            }
+            var eventTypes = {
+                input: 'input',
+                textarea: 'keyup',
+                select: 'change'
+            };
+            var listenFor = eventTypes[ tag ];
+            field.addEventListener( listenFor, function ( e ) {
+                var obj = fsValidate( e.target.value );
+                if ( obj.valid ) {
+                    Cookies.set( siteName, obj.value );
+                    Cookies.set( pageName, obj.value );
+                }
+            } );
+        } );
+    }
+
+};
+
